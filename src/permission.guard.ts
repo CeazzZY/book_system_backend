6eca51acb18dd51a6344d4b4b152fac05d5976b3
CeazzZY <1093918683@qq.com>
2024-05-14 21:01:13 +0800

    feat: permissionGaurd

--- src/permission.guard.ts
+++ src/permission.guard.ts
@@ -1,11 +1,43 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  @Inject()
+  private reflector: Reflector;
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    if (!request.user) {
+      return true;
+    }
+
+    const permissions = request.user.permissions;
+
+    const requiredPermissions = this.reflector.getAllAndOverride<string[]>(
+      'require-permission',
+      [context.getClass(), context.getHandler()],
+    );
+
+    if (!requiredPermissions) {
+      return true;
+    }
+
+    for (let i = 0; i < requiredPermissions.length; i++) {
+      const curPermission = requiredPermissions[i];
+      const found = permissions.find((item) => item.code === curPermission);
+      if (!found) {
+        throw new UnauthorizedException('你没有权限');
+      }
+    }
+
     return true;
   }
 }
