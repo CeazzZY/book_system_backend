8bd454af41ca258e656d4779c81066d37730e716
CeazzZY <1093918683@qq.com>
2024-05-14 21:10:17 +0800

    feat: format-response-interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { FormatResponseInterceptor } from './format-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('nest_server_port'));
