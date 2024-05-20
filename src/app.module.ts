9ff637b3cbccd4330e0daeeb47e2110458defb22
CeazzZY <1093918683@qq.com>
2024-05-20 16:02:17 +0800

    feat: booking

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,8 @@ import { LoginGuard } from './login.guard';
 import { PermissionGuard } from './permission.guard';
 import { MeetingRoomModule } from './meeting-room/meeting-room.module';
 import { MeetingRoom } from './meeting-room/entities/meeting-room.entity';
+import { BookingModule } from './booking/booking.module';
+import { Booking } from './booking/entities/booking.entity';
 
 @Module({
   imports: [
@@ -45,7 +47,7 @@ import { MeetingRoom } from './meeting-room/entities/meeting-room.entity';
           database: configService.get('mysql_server_database'),
           synchronize: true,
           logging: true,
-          entities: [User, Role, Permission, MeetingRoom],
+          entities: [User, Role, Permission, MeetingRoom, Booking],
           poolSize: 10,
           connectorPackage: 'mysql2',
           extra: {
@@ -59,6 +61,7 @@ import { MeetingRoom } from './meeting-room/entities/meeting-room.entity';
     RedisModule,
     EmailModule,
     MeetingRoomModule,
+    BookingModule,
   ],
   controllers: [AppController],
   providers: [
